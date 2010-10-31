8858891b51dff48815148717748f74eceb488791
michaelrmartinezm <michaelrmartinezm@e1bb0972-c026-d1ed-a952-b9284acf5ba6>
2010-10-31 02:40:21 +0000

    Este es el prototipo para el caso de uso de facturar el pago de tarifa aeroportuaria por pasaje a la aerolínea. Este prototipo fue elaborado por Michael Martínez y Carlos García. El prototipo fue elaborado el día sábado 29 de Octubre.

---
+++ Construcción/UPC.Trabajo.KBHit/frmFacturacionTUA.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace UPC.Trabajo.KBHit
+{
+    public partial class frmFacturacionTUA : Telerik.WinControls.UI.ShapedForm
+    {
+        public frmFacturacionTUA()
+        {
+            InitializeComponent();
+        }
+    }
+}
